1cc0b312189a6de8f6d9de6299ba614d8ae0f0e4
LordNani <47566320+LordNani@users.noreply.github.com>
2020-06-06 12:33:25 +0300

    Some tests with client id and duel

--- bot.js
+++ bot.js
@@ -1,50 +1,65 @@
-const tmi = require('tmi.js');
-
+/* eslint-disable new-cap */
+const tmi = require('tmi.js')
+const axios = require('axios');
 // Define configuration options
 const opts = {
   identity: {
-    username: "BotLord",
-    password: "s5w5yitijwbrwhnd24zaqg6rf5odda"
+    username: 'BotLord',
+    password: 's5w5yitijwbrwhnd24zaqg6rf5odda'
   },
   channels: [
-    "lord_painkiller"
+    'lord_painkiller'
   ]
-};
+}
 
 // Create a client with our options
-const client = new tmi.client(opts);
+const client = new tmi.client(opts)
 
 // Register our event handlers (defined below)
-client.on('message', onMessageHandler);
-client.on('connected', onConnectedHandler);
+client.on('message', onMessageHandler)
+client.on('connected', onConnectedHandler)
 
 // Connect to Twitch:
-client.connect();
+client.connect()
 
 // Called every time a message comes in
-function onMessageHandler (target, context, msg, self) {
-  if (self) { return; } // Ignore messages from the bot
+async function onMessageHandler (target, context, msg, self) {
+  if (self) { return } // Ignore messages from the bot
 
   // Remove whitespace from chat message
-  const commandName = msg.trim();
+  const commandArray = msg.trim().split(' ')
+  console.log('The executed command is: ' + commandArray)
 
-  
   // If the command is known, let's execute it
-  if (commandName === '!dice') {
-    const num = rollDice();
-    client.say(target, `You rolled a ${num}`);
-    console.log(`* Executed ${commandName} command`);
+  if (commandArray[0] === '!duel') {
+    const opponent = await startDuel(commandArray[1])
+    client.say(target, opponent)
+    console.log(`* Executed ${commandArray} command`)
   } else {
-    console.log(`* Unknown command ${commandName}`);
+    console.log(`* Unknown command ${commandArray}`)
   }
 }
-// Function called when the "dice" command is issued
-function rollDice () {
-  const sides = 6;
-  return Math.floor(Math.random() * sides) + 1;
+// Function called when the 'dice' command is issued
+async function startDuel (userName) {
+   return axios.get('https://api.twitch.tv/kraken/users?login=' + userName).then(
+    response => response.json()
+  ).then(
+    json => {
+      if (json.users._id != null) {
+        return 'User Exists'
+      } else {
+        return "User Doesn't Exist"
+      }
+    }
+  ).catch(
+    reason => {
+        // console.log(reason)
+        return "User Doesn't Exist"
+    }
+  )
 }
 
 // Called every time the bot connects to Twitch chat
 function onConnectedHandler (addr, port) {
-  console.log(`* Connected to ${addr}:${port}`);
+  console.log(`* Connected to ${addr}:${port}`)
 }
